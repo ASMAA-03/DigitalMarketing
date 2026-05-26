b8486e17e8c892f2446241150a9874ec6da4c223
ASMAA-03 <asmaaaldabbas5@gmail.com>
2026-05-26 22:39:28 +0300

    add herosection comp and fix navbar

--- digitalmarketing/src/app/(public)/onbording/page.jsx
+++ digitalmarketing/src/app/(public)/onbording/page.jsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/herosection";
 export default function HomePage() {
   return (
     <>
     <Navbar/>
+    <HeroSection/>
     </>
   );
 }
